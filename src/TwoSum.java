9ed89e1bfbe9942b1bd86541e7fc074930424315
Kanishk Kumar <kanishkkumarr17@gmail.com>
2025-09-05 16:02:47 -0700

    Adding two sums

---
+++ src/TwoSum.java
@@ -0,0 +1,13 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i<n; i++){
+            for (int j = n+1; j<i; j++){
+                if (nums[i]+ nums[j]== target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
